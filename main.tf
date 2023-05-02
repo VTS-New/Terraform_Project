b217e2b5cf230a4f0ce199ea7e802044c5bca1f8
S2193 <97331479+S2193@users.noreply.github.com>
2023-05-02 12:36:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+    version = "~> 4.39.0"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+}
+
+data "aws_availability_zones" "azs" {
+    state = "available"
+}
